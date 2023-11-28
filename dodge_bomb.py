60d48d9d08a2ff01237ddf099cc9ba5a86a1d810
c0a22134 <c0a2213421@edu.teu.ac.jp>
2023-11-28 14:46:56 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,12 @@ import random
 
 WIDTH, HEIGHT = 1600, 900
 
+delta = {
+    pg.K_UP:(0, -5),
+    pg.K_DOWN:(0, +5),
+    pg.K_LEFT:(-5, 0),
+    pg.K_RIGHT:(+5, 0)
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -11,6 +17,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bb_img = pg.Surface((20, 20))
     bb_img.set_colorkey((0,0,0))
     pg.draw.circle(bb_img, (255,0,0), (10, 10), 10)
@@ -25,9 +33,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for k, tpl in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
