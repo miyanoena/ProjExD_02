68eb3aba89aa2327adb1e6da323e93262be9cb65
c0a22134 <c0a2213421@edu.teu.ac.jp>
2023-11-28 14:59:04 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -42,6 +42,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
             
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
